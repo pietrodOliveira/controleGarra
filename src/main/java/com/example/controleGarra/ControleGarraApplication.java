4080be1f8389ec1939878bea893eb67844f53e05
Pipo <pietrocorona16@gmail.com>
2024-11-14 13:54:03 -0300

    first commit

---
+++ src/main/java/com/example/controleGarra/ControleGarraApplication.java
@@ -0,0 +1,13 @@
+package com.example.controleGarra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleGarraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleGarraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/controleGarra/ControleGarraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controleGarra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleGarraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
